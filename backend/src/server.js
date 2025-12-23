f167534fba8bd4de41e60293d2314cc5ca0b95ee
surajbhatt27 <bhattsuraj0066@gmail.com>
2025-12-23 20:37:50 +0530

    chore: add cors middleware to backend and update package.json dependencies

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,7 +1,13 @@
 import express from 'express'
 import {ENV} from './lib/env.js'
+import cors from 'cors'
 
 const app = express()
+
+app.use(cors({
+    origin: "http://localhost:5173",
+    credentials: true
+}));
 
 app.get('/health', (req, res) => {
     res.status(200).json({msg: "Server is up and running"})

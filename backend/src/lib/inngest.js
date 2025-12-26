abc39a8bf97652b0d27caf2f521f49e9796d7ed5
surajbhatt27 <bhattsuraj0066@gmail.com>
2025-12-26 17:33:47 +0530

    fix: ensure database connection is established in syncUser function

--- backend/src/lib/inngest.js
+++ backend/src/lib/inngest.js
@@ -8,7 +8,7 @@ const syncUser = inngest.createFunction(
     {id: 'sync-user'},
     {event: 'clerk/user.created'},
     async ({event}) => {
-        //await connectDB()
+        await connectDB()
 
         const {id, email_addresses, first_name, last_name, image_url} = event.data
 

1748cbde47c824ce6bdc435c8553e926336c2d09
surajbhatt27 <bhattsuraj0066@gmail.com>
2025-12-26 17:29:50 +0530

    fix: correct function name and email field in inngest.js

--- backend/src/lib/inngest.js
+++ backend/src/lib/inngest.js
@@ -4,17 +4,17 @@ import User from '../models/User.js'
 
 export const inngest = new Inngest({ id: "hireloop" })
 
-const synceUser = inngest.createFunction(
+const syncUser = inngest.createFunction(
     {id: 'sync-user'},
     {event: 'clerk/user.created'},
     async ({event}) => {
-        await connectDB()
+        //await connectDB()
 
         const {id, email_addresses, first_name, last_name, image_url} = event.data
 
         const newUser = {
             clerkId: id,
-            email: email_addresses[0]?.email_addresses,
+            email: email_addresses[0]?.email_address,
             name: `${first_name} ${last_name}`,
             profileImage: image_url
         }
@@ -34,4 +34,4 @@ const deleteUserFromDB = inngest.createFunction(
     }
 )
 
-export const functions = [synceUser, deleteUserFromDB]
+export const functions = [syncUser, deleteUserFromDB]

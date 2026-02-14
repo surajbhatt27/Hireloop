0948725192d9ef8e7736a9b7f7ec401209285bbb
surajbhatt27 <bhattsuraj0066@gmail.com>
2026-02-14 20:38:08 +0530

    temp test line

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -4,5 +4,6 @@ const axiosInstance = axios.create({
     baseURL: import.meta.env.VITE_API_URL,
     withCredentials: true //browser will send cookies to server automatically (on every single req)
 })
+console.log("VITE_API_URL =", import.meta.env.VITE_API_URL)
 
 export default axiosInstance
